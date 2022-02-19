ef8f4c24ecf2aabc288ab6ed74bdc21b9da1013f
Majid Alfreidi <majidalfredi123@gmail.com>
2022-02-19 10:43:28 +0700

    Menambahkan Pertemuan 2

---
+++ Pertemuan2/index.php
@@ -0,0 +1,27 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// sintaks php
+
+//standar output
+// echo, print
+// print_r
+// var_dump
+
+// penulisan sintaks php
+// 1. PHP Didalam html
+// 2. html didalam php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar PHP </title>
+</head>
+<body>
+        <h1>Hallo Selamat datang <?php echo "Majid"; ?></h1>
+    
+</body>
+</html>
